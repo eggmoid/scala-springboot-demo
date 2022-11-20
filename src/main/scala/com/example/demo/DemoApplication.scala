3f35d6a802db578ab8d30264ecd678f258bc0b6f
임준영 <limejuny@gmail.com>
2022-11-20 20:20:45 +0900

    Scala to Java
    Detail
    ======
     - Scala 3.2.0

---
+++ src/main/scala/com/example/demo/DemoApplication.scala
@@ -0,0 +1,10 @@
+package com.example.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication
+
+@main
+def main(): Unit = SpringApplication.run(classOf[DemoApplication])
